ad434fa268a1ef2f63aa874c302cc1c51e7827f6
zach ennenga <astrozees@gmail.com>
2025-07-23 00:23:14 -0700

    Fix gg's sick code because zach is neurotic

---
+++ tests/constant/test_type_chart_completeness.py
@@ -0,0 +1,13 @@
+import unittest
+
+from constants.type_chart import TYPE_EFFECTIVENESS
+from constants.types import PokemonType
+
+
+class TestTypeChartCompleteness(unittest.TestCase):
+    def test_type_chart_completeness(self):
+        for type_ in PokemonType:
+            with self.subTest(f"test_type_chart_completeness_{type_.name}"):
+                chart = TYPE_EFFECTIVENESS[type_]
+                for test_type in PokemonType:
+                    self.assertIn(test_type, chart)
